cffeb6ec5124a0ed96fc4e5dcb73522c8adf4fff
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-10-29 14:19:42 +0530

    add a constructor method to Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,5 +2,11 @@ package thread.tortoise;
 
 public class Tortoise extends Thread {
     private static final int MILESTONE = 5;
-    private float timeTakenByTortoiseToCompleteTheRace;
+    private float timeTakenByTortoiseToCompleteTheRace = 0f;
+
+    private final Thread tortoise;
+
+    public Tortoise(Thread tortoise) {
+        this.tortoise = tortoise;
+    }
 }
