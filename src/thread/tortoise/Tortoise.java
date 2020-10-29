b1c9f95e5f9646a018217b165df5d8100ddd5d3d
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-10-29 14:30:30 +0530

    add getter and setter method to the variable timeTakenByTortoiseToCompleteTheRace in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,11 +2,28 @@ package thread.tortoise;
 
 public class Tortoise extends Thread {
     private static final int MILESTONE = 5;
-    private float timeTakenByTortoiseToCompleteTheRace = 0f;
-
     private final Thread tortoise;
+    private float timeTakenByTortoiseToCompleteTheRace = 0f;
 
     public Tortoise(Thread tortoise) {
         this.tortoise = tortoise;
+    }
+
+    /**
+     * This method return the time taken by tortoise to finish his race.
+     *
+     * @return time taken by tortoise to complete the race
+     */
+    public float getTimeTakenByTortoiseToCompleteTheRace() {
+        return timeTakenByTortoiseToCompleteTheRace;
+    }
+
+    /**
+     * This method sets the value of time taken by tortoise to finish his race
+     *
+     * @param timeTakenByTortoise time taken by tortoise to complete the race
+     */
+    public void setTimeTakenByTortoiseToCompleteTheRace(float timeTakenByTortoise) {
+        this.timeTakenByTortoiseToCompleteTheRace = timeTakenByTortoise;
     }
 }
