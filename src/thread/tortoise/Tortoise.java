f2c6247b74fd7ac63d331d274e53d7fda77880fe
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-10-29 14:38:57 +0530

    override the run method in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,7 +1,9 @@
 package thread.tortoise;
 
+import javax.swing.*;
+
 public class Tortoise extends Thread {
-    private static final int MILESTONE = 5;
+    private static final int MILESTONES = 5;
     private final Thread tortoise;
     private float timeTakenByTortoiseToCompleteTheRace = 0f;
 
@@ -29,5 +31,41 @@ public class Tortoise extends Thread {
 
     public Thread getTortoise() {
         return tortoise;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+                timeTakenByTortoiseToCompleteTheRace = timeTakenByTortoiseToCompleteTheRace + 3f;
+
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
     }
 }
